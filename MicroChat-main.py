c3b4e7d9f744780a879cf05823956f8172483d80
Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2026-07-24 13:08:32 -0300

    Agregar archivos principales de MicroChat y Pruebas, incluyendo lógica de comunicación y animaciones

---
+++ MicroChat-main.py
@@ -0,0 +1,225 @@
+# Imports go at the top
+from microbit import *
+import radio
+import machine
+
+radio.config(channel=2, length=250)
+radio.on()
+uart.init(baudrate=115200)
+
+serial_buffer = ""
+
+# Estados: 0 - inicial, 1 - sesion iniciada, 2 - inciando sesión
+estado = 0
+
+# SubEstados: 1 - Menu Principal, 2 - Chat, 3 - Lista de Usuarios
+subEstado = 0
+
+marcaTiempo=0
+subMarcaTiempo=0
+nombre = ''
+placa = machine.unique_id()
+mensaje = ''
+intentos = 0
+usuarios=[]
+placas=[]
+
+def animacionRecibir():
+    display.show(Image('00000:'
+                       '09090:'
+                       '00900:'
+                       '00000:'
+                       '00000'))
+    sleep(150)
+    display.clear()
+    
+    
+def animacionEnviar():
+    display.show(Image('00000:'
+                       '00000:'
+                       '00900:'
+                       '09090:'
+                       '00000'))
+    sleep(150)
+    display.clear()
+
+def enviarMensaje(orden,valor):
+    radio.send(orden+':'+valor)
+    animacionEnviar()
+
+#Imprime una línea decorativa
+def linea():
+    uart.write("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\r\n")
+
+def lineaDecorada():
+    uart.write("mÖ -- -- -- -- -- -- -- -- -- -- -- -- -- -- Öm\r\n")
+
+
+def menuPrincipal():
+    ##global nombre,usuarios,estado
+    ##opcion=0
+    ##while opcion!=3:
+    lineaDecorada()
+    escribir("Menú Principal")
+    linea()
+    ('1 - Ir al Chat')
+    escribir('2 - Mostrar Usuarios')
+        #opcion=input('Opción:')
+        
+    linea()
+    #    if opcion=='2':
+    #        escribir('Usuarios Conectados: ' +str(len(usuarios)+1) )
+     #       linea()
+      #      escribir(' ')
+       #     escribir('Yo: '+nombre)
+        #    for usuario in usuarios:
+         #       escribir('- '+usuario)
+          #  linea()
+        #if opcion=='1':
+         #   estado=1
+          #  opcion=3
+            
+
+    
+
+def evaluarComando(comando):
+    cosa=""
+
+
+    """Permite enviar un texto a la consola serial
+    """
+def escribir(texto):
+    uart.write(texto + '\r\n')
+    
+def evaluarMensaje(mensaje):
+    global nombre, estado, subEstado, intentos
+    orden=mensaje[0:(mensaje.find(':'))]
+    valor=mensaje[ (mensaje.find(':'))+1:len(mensaje)]
+
+    if (orden=='nuevoLogin'):
+        if(valor==nombre):
+            enviarMensaje('nombreRepetido','')
+    if (orden=='nombreRepetido'):
+        escribir('ERROR: Ese nombre ya está en uso')
+        escribir('Intenta nuevamente con otro nombre')
+        linea()
+        estado=0
+    if(orden=='holaSoy'):
+        usuario=valor[0:(valor.find(':'))]
+        idPlaca=valor[(valor.find(':'))+1:len(valor)]
+        if usuarios.count(usuario)<1:
+            usuarios.append(usuario)
+            placas.append(idPlaca)
+            enviarMensaje('holaSoy',nombre+':'+placa)
+    if (orden=='m'):
+        if estado==1 and subEstado==2:
+            usuario=valor[0:(valor.find(':'))]
+            msj=valor[(valor.find(':'))+1:len(valor)]
+            escribir(usuario+':'+msj)
+            linea()
+
+
+# Interfaz de usuario inicial
+display.show(Image( '06060:'
+                    '09090:'
+                    '90009:'
+                    '96069:'
+                    '09990'))
+
+
+if estado == 0:
+    lineaDecorada()
+    escribir("                 MicroChat v1")
+    linea()
+    escribir("Hola")
+    escribir("¿Cuál es tu nombre?")
+    linea()
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    
+    # Recepción de mensajes - Siempre activa
+    mensaje = radio.receive()
+    if mensaje:
+        #escribir(mensaje)
+        animacionRecibir()
+        evaluarMensaje(mensaje)
+        
+    if estado == 2:
+        if running_time() < marcaTiempo:
+            if running_time() >= subMarcaTiempo:
+                escribir("Conectando ...")
+                subMarcaTiempo = running_time()+1000
+        else:
+                escribir('¡¡Listo!!')
+                estado = 1
+                linea()
+                escribir('Hola, '+nombre)
+                escribir('Te uniste a la red MicroChat')
+                escribir('')
+                menuPrincipal()
+    
+    
+    # Manejo de comandos desde el puerto serial/consola
+    if uart.any():
+        # Obtener el primer caracter de la conexión
+        char_bytes = uart.read(1)
+        
+        if char_bytes:
+            # Conversión a caracteres de texto
+            char = str(char_bytes, 'UTF-8')
+            
+            # Análisis: Si el caracter se corresponde con 'Enter' (\r o \n)
+            #   se evalúa el comando recibido
+            if char == '\n' or char == '\r':
+                if serial_buffer:
+                    #escribir("\n[Serial Command Received]:", serial_buffer)
+                    escribir('')
+                    
+                    if estado == 0:
+                        nombre = serial_buffer
+                        enviarMensaje('nuevoLogin',nombre)
+                        estado = 2
+                        marcaTiempo=running_time()+3000
+                        subMarcaTiempo=running_time()+1000
+                        linea()
+                        escribir("Conectando a la red MicroChat como "+nombre+"...")
+                        
+                        
+                    elif estado == 1:
+                        if serial_buffer=='1':
+                            subEstado=2
+                            lineaDecorada()
+                            escribir('CHAT')
+                            linea()
+                            escribir('Ingresa tu mensaje y presiona Enter para enviarlo')
+                            linea()
+                    
+                        
+                    # Reset the string container for the next phrase
+                    uart.write("\r\n")
+                    #escribir('Estado = '+str(estado))
+                    serial_buffer = ""
+            else:
+                # Echo the user's keystroke back to the terminal window
+                uart.write(char)
+                serial_buffer += char
+
+    # Microsecond pause to keep processing stable
+    sleep(10)
+    
+#    if (estado == 0):
+#        linea()    
+#        uart.write("MicroChat v1")
+#        linea()
+#        uart.write('Hola')
+        #nombre = input
+#        uart.write('¿Cuál es tu nombre?: ')
+        #login(nombre)
+#    if (estado == 1):
+#        menuPrincipal()
+    
+    
+        
+
+
---
+++ Pruebas-main.py
@@ -0,0 +1,11 @@
+from microbit import *
+
+while True:
+    display.show(Image('00000:'
+                           '00000:'
+                           '00900:'
+                           '09090:'
+                           '00000'))
+    sleep(150)
+    display.clear()
+    sleep(200)
